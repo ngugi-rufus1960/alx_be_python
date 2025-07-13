9304530bc40c9d8c760bd90391d6f94484a81946
ngugi-rufus1960 <124779898+ngugi-rufus1960@users.noreply.github.com>
2025-07-13 23:55:33 +0300

    Update finance_calculator.py

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,5 @@
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
+monthly_savings = monthly_income - monthly_expenses
+annual_savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+print(f"Your annual savings, including intrests, is:{annual_savings}")
