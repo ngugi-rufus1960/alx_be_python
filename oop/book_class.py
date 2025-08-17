0f6a81a82dea8d5ce080a64139730c7e4597ce50
ngugi-rufus1960 <124779898+ngugi-rufus1960@users.noreply.github.com>
2025-08-17 22:33:50 +0300

    Update book_class.py

---
+++ oop/book_class.py
@@ -0,0 +1,20 @@
+# oop/book_class.py
+
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """Constructor: Initialize a Book instance."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor: Called when an object is deleted."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """String representation: User-friendly description of the book."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Official representation: Can recreate the object."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
